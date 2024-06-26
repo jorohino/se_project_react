3d02048fbf39d7d41a45c968e9db3ca39a23cb57
jorohino <149121790+jorohino@users.noreply.github.com>
2024-06-26 14:42:51 -0400

    Implement ItemModal and ModalWithForm CSS styles

--- se_project_react/src/components/ItemModal/ItemModal.jsx
+++ se_project_react/src/components/ItemModal/ItemModal.jsx
@@ -9,10 +9,12 @@ function ItemModal({ activeModal, onClose, card }) {
           className="modal__close"
           type="button"
         ></button>
-        <img src={card.link} alt="" className="modal__image" />
+        <img src={card.link} alt="card.name" className="modal__image" />
         <div className="modal__footer">
-          <h2 className="modal__caption">{card.name}</h2>
-          <p className="modal__weather">Weather: {card.weather}</p>
+          <div className="modal__footer-info">
+            <h2 className="modal__caption">{card.name}</h2>
+            <p className="modal__weather">Weather: {card.weather}</p>
+          </div>
         </div>
       </div>
     </div>
