a9efa680c15a7169797987b6c9ec9864159e0698
jorohino <149121790+jorohino@users.noreply.github.com>
2024-06-26 12:57:06 -0400

    Implement WeatherCard CSS styles

--- se_project_react/src/components/WeatherCard/WeatherCard.jsx
+++ se_project_react/src/components/WeatherCard/WeatherCard.jsx
@@ -4,7 +4,7 @@ import "./WeatherCard.css";
 function WeatherCard({ weatherData }) {
   return (
     <section className="weather-card">
-      <p className="weather-card__temp">{weatherData.temp.F} &deg; F</p>
+      <p className="weather-card__temp">{weatherData.temp.F}&deg;F</p>
       <img src={sunny} alt="sunny" className="weather-card__image" />
     </section>
   );
