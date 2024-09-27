a2e56e512a9abd474d1f173580fc0632f92d870c
jorohino <149121790+jorohino@users.noreply.github.com>
2024-09-27 13:29:12 -0400

    Rename AppContext to CurrentUserContext. Implement basic login logic integration to App.jsx. Wrap all content in CurrentUserContext, wrap /profile in ProtectedRoute.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
+import * as auth from "../../utils/auth";
 
 import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Profile from "../Profile/Profile";
 import Footer from "../Footer/Footer";
-// import ModalWithForm from "../ModalWithForm/ModalWithForm";
 import ItemModal from "../ItemModal/ItemModal";
 import { getWeather, filterWeatherData } from "../../utils/weatherApi";
 import { APIkey, coordinates } from "../../utils/constants";
@@ -16,6 +18,9 @@ import { getItems, addItem, deleteItem } from "../../utils/api";
 import ConfirmDeleteModal from "../ConfirmDeleteModal/ConfirmDeleteModal";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  //TODO: const [userData, setUserData] = useState({})
+
   const [weatherData, setWeatherData] = useState({
     type: "",
     temp: { F: 999 },
@@ -25,6 +30,22 @@ function App() {
   const [selectedCard, setSelectedCard] = useState({});
   const [currentTemperatureUnit, setCurrentTemperatureUnit] = useState("F");
   const [clothingItems, setClothingItems] = useState([]);
+
+  const handleLogin = ({ email, password }) => {
+    if (!username || !password) {
+      return;
+    }
+
+    /*auth
+      .authorize(email, password)
+      .then((data) => {
+        if (data.jwt);
+        setUserData(data.user);
+        setIsLoggedIn(true);
+
+        const redirectPath = location.state?.from?.pathname
+      }) */
+  };
 
   const handleCardClick = (card) => {
     setActiveModal("preview");
@@ -91,55 +112,59 @@ function App() {
   }, []);
 
   return (
-    <div className="page">
-      <CurrentTemperatureUnitContext.Provider
-        value={{ currentTemperatureUnit, handleToggleSwitchChange }}
-      >
-        <div className="page__content">
-          <Header handleAddClick={handleAddClick} weatherData={weatherData} />
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <Main
-                  weatherData={weatherData}
-                  onCardClick={handleCardClick}
-                  clothingItems={clothingItems}
-                />
-              }
-            />
-            <Route
-              path="/profile"
-              element={
-                <Profile
-                  onCardClick={handleCardClick}
-                  clothingItems={clothingItems}
-                  handleAddClick={handleAddClick}
-                />
-              }
-            />
-          </Routes>
-          <Footer />
-        </div>
-        <AddItemModal
-          closeActiveModal={closeActiveModal}
-          isOpen={activeModal === "add-garment"}
-          onAddItem={onAddItem}
-        />
-        <ItemModal
-          isOpen={activeModal === "preview"}
-          card={selectedCard}
-          onClose={closeActiveModal}
-          onDeleteItem={handleDeleteClick}
-        />
-        <ConfirmDeleteModal
-          isOpen={activeModal === "confirm-delete"}
-          card={selectedCard}
-          closeActiveModal={closeActiveModal}
-          onDeleteItem={onDeleteItem}
-        />
-      </CurrentTemperatureUnitContext.Provider>
-    </div>
+    <CurrentUserContext.Provider value={{ isLoggedIn }}>
+      <div className="page">
+        <CurrentTemperatureUnitContext.Provider
+          value={{ currentTemperatureUnit, handleToggleSwitchChange }}
+        >
+          <div className="page__content">
+            <Header handleAddClick={handleAddClick} weatherData={weatherData} />
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <Main
+                    weatherData={weatherData}
+                    onCardClick={handleCardClick}
+                    clothingItems={clothingItems}
+                  />
+                }
+              />
+              <Route
+                path="/profile"
+                element={
+                  <ProtectedRoute>
+                    <Profile
+                      onCardClick={handleCardClick}
+                      clothingItems={clothingItems}
+                      handleAddClick={handleAddClick}
+                    />
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+            <Footer />
+          </div>
+          <AddItemModal
+            closeActiveModal={closeActiveModal}
+            isOpen={activeModal === "add-garment"}
+            onAddItem={onAddItem}
+          />
+          <ItemModal
+            isOpen={activeModal === "preview"}
+            card={selectedCard}
+            onClose={closeActiveModal}
+            onDeleteItem={handleDeleteClick}
+          />
+          <ConfirmDeleteModal
+            isOpen={activeModal === "confirm-delete"}
+            card={selectedCard}
+            closeActiveModal={closeActiveModal}
+            onDeleteItem={onDeleteItem}
+          />
+        </CurrentTemperatureUnitContext.Provider>
+      </div>
+    </CurrentUserContext.Provider>
   );
 }
 
--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,11 +1,11 @@
 import { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import AppContext from "../../contexts/AppContext";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
 
 function ProtectedRoute({ children, anonymous = false }) {
   const location = useLocation();
   const from = location.state?.from || "/";
-  const { isLoggedIn } = useContext(AppContext);
+  const { isLoggedIn } = useContext(CurrentUserContext);
 
   if (anonymous && isLoggedIn) {
     return <Navigate to={from} />;
